c3430adad1e06ac0abc83450a16a81c4fa800e00
Christina Nathan <christina.a.nate@gmail.com>
2021-11-18 14:19:45 -0800

    complete

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ app.use(bodyParser.json());
 //server urls will exist in env file in production.
 require('dotenv').config();
 
-const servers = ['http://ec2-3-145-57-143.us-east-2.compute.amazonaws.com:3030'];
+const servers = process.env.SERVERS.split(',');
+console.log(servers);
 let current = 0;
 
 app.get('/*', (req, res) => {
   console.log('current server: ', servers[current]);
   console.log('req.url: ', req.url);
-  console.log('req.body: ', req.body);
   console.log('req.query: ', req.query);
   const url = req.url;
 
   axios.get(servers[current] + url)
     .then(results => {
       console.log('success!');
+      current = (current + 1) % servers.length;
+      console.log(current);
       res.status(200).send(results.data);
     })
     .catch(err => {
@@ -35,11 +37,12 @@ app.post('/*', (req, res) => {
   console.log('current server: ', servers[current]);
   console.log('req.url: ', req.url);
   console.log('req.body: ', req.body);
-  console.log('req.query: ', req.query);
 
   axios.post(servers[current] + req.url, req.body)
     .then(results => {
       console.log('success post');
+      current = (current + 1) % servers.length;
+      console.log(current);
       res.status(201).send(results.data);
     })
     .catch(err => {
@@ -50,9 +53,17 @@ app.post('/*', (req, res) => {
 app.put('/*', (req, res) => {
   console.log('current server: ', servers[current]);
   console.log('req.url: ', req.url);
-  console.log('req.body: ', req.body);
-  console.log('req.query: ', req.query);
-  res.send('Checking setup');
+
+  axios.put(servers[current] + req.url)
+    .then(results => {
+      console.log('success put');
+      current = (current + 1) % servers.length;
+      console.log(current);
+      res.status(200).send(results.data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 });
 
 app.listen(port, () => {
